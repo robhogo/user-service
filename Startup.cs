48b3edfc74fbfdb37379578eb7b053fb1fe59b55
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-05-10 16:21:05 +0200

    appsettings voor deployment added

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace RoBHo_UserService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration.GetConnectionString("UserServiceContext");
+            var connection = Configuration.GetValue<string>("ConnectionString");
             services.AddDbContext<UserServiceContext>(
                 options => options.UseSqlServer(connection));
 
